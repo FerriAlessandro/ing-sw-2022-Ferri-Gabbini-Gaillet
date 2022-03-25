76fc15eea7e8a5611bc8e735a63e5e4241d663da
alessandroGabbini <alessandro.gabbini@mail.polimi.it>
2022-03-25 14:39:45 +0100

    add color enumerations

---
+++ src/main/java/it/polimi/ingsw/model/enumerations/Color.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.model.enumerations;
+
+/**
+ * This enumeration provides colors both for students and professors.
+ */
+
+public enum Color {
+    RED,
+    BLUE,
+    PINK,
+    YELLOW,
+    GREEN
+}
---
+++ src/main/java/it/polimi/ingsw/model/enumerations/TowerColor.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.model.enumerations;
+
+/**
+ * This enumeration provides colors for towers.
+ */
+
+public enum TowerColor {
+    GRAY,
+    BLACK,
+    WHITE
+}
