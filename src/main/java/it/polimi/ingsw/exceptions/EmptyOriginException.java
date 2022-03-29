96255eef4f041e482872a5300ff2c00308a2aba8
Angelo Giovanni Gaillet <angelogiovanni.gaillet@mail.polimi.it>
2022-03-29 09:46:04 +0200

    Add custom exceptions

---
+++ src/main/java/it/polimi/ingsw/exceptions/EmptyOriginException.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.exceptions;
+
+/**
+ * Custom exception to be thrown were the origin of a move or remove is already empty.
+ * @author A.G. Gaillet
+ * @version 1.0
+ */
+public class EmptyOriginException extends Exception{
+    public EmptyOriginException(){
+        super("Given origin tile is empty");
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/exceptions/FullDestinationException.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.exceptions;
+
+/**
+ * Custom exception to be thrown when the given destination for a move or add is already full.
+ * @author A.G. Gaillet
+ * @version 1.0
+ */
+public class FullDestinationException extends Exception{
+    public FullDestinationException(){
+        super("Given destination tile is full");
+    }
+}
