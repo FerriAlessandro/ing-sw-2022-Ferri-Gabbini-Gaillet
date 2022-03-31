f56aa229b18d596cd41c91d040105969b1ab90af
FerriAlessandro <100209676+FerriAlessandro@users.noreply.github.com>
2022-03-31 21:46:25 +0200

    Fixed javadoc position

--- src/main/java/it/polimi/ingsw/exceptions/NoCurrentPlayerException.java
+++ src/main/java/it/polimi/ingsw/exceptions/NoCurrentPlayerException.java
@@ -1,11 +1,13 @@
 package it.polimi.ingsw.exceptions;
 
-public class NoCurrentPlayerException extends Exception{
 /**
  * Exception thrown if it's nobody's turn.
  * @author Alessandro F.
  * @version 1.0
  */
+
+public class NoCurrentPlayerException extends Exception{
+
     public NoCurrentPlayerException(){
 
         super("ERROR! There's not a current player");
